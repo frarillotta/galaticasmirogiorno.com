74f90ad3e0417ebfe2f4ea4f40cc17e41514abf1
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-05-06 19:58:03 +0100

    lets get greedy

--- src/pages/projects/1/index.tsx
+++ src/pages/projects/1/index.tsx
@@ -8,65 +8,26 @@ type ProjectImageProps = {
   projNumber: number;
   width: number;
   height: number;
+  priority?: boolean;
+  loading?: 'eager' | 'lazy';
   className?: string;
 }
 
 //TODO: add label to each of these
-const ProjectImage: React.FC<ProjectImageProps> = ({ pictureName, projNumber, width, height, className = '' }) => {
+const ProjectImage: React.FC<ProjectImageProps> = ({ pictureName, projNumber, width, height, loading, priority = false, className = '' }) => {
   return <div className={`${styles.pictureWrapper} ${className}`}>
-    <Image className={styles.projectPicture} alt={`project ${projNumber} ${pictureName} Picture`} src={`/projects/${projNumber}/pictures/${pictureName}`} width={width} height={height} />
+    <Image 
+      loading={loading} 
+      placeholder='blur' 
+      priority={priority}
+      className={styles.projectPicture} 
+      alt={`project ${projNumber} ${pictureName} Picture`} 
+      src={`/projects/${projNumber}/pictures/${pictureName}`} 
+      width={width} 
+      height={height} 
+    />
   </div>
 };
-
-const projectPictures = [
-  {
-    width: 65,
-    height: 117,
-    name: 'blueprint',
-    className: styles.blueprint
-  },
-  {
-    width: 257,
-    height: 86,
-    name: 'front',
-    className: styles.front
-  }, {
-    width: 260,
-    height: 171,
-    name: 'handle',
-    className: styles.handle
-  }, {
-    width: 256,
-    height: 355,
-    name: 'inside',
-    className: styles.inside
-  }, {
-    width: 220,
-    height: 434,
-    name: 'mirror1',
-    className: styles.mirror1
-  }, {
-    width: 220,
-    height: 434,
-    name: 'mirror2',
-    className: styles.mirror2
-  }, {
-    width: 220,
-    height: 434,
-    name: 'mirror3',
-    className: styles.mirror3
-  }, {
-    width: 220,
-    height: 434,
-    name: 'mirror4',
-    className: styles.mirror4
-  }, {
-    width: 220,
-    height: 434,
-    name: 'mirror5',
-    className: styles.mirror5
-  },
-]
 
 export default function Project1() {
   return (
@@ -92,22 +53,22 @@ export default function Project1() {
               <span>A HAIR SALON WITH FAKE BROKEN MIRRORS.</span><br />
             </p>
           </div>
-          <ProjectImage className={styles.oldFront} pictureName={'front-old.jpg'} height={1178} width={790} projNumber={1}></ProjectImage>
+          <ProjectImage loading='eager' priority={true} className={styles.oldFront} pictureName={'front-old.jpg'} height={1178} width={790} projNumber={1}></ProjectImage>
         </AnimatePresence>
       </article>
       <article className={styles.screen}>
-        <ProjectImage className={styles.front} pictureName={'front.jpg'} height={573} width={843} projNumber={1}></ProjectImage>
+        <ProjectImage loading='eager' className={styles.front} pictureName={'front.jpg'} height={573} width={843} projNumber={1} />
       </article>
       <article className={styles.screen}>
-        <ProjectImage className={styles.layout} pictureName={'layout.png'} height={870} width={830} projNumber={1}></ProjectImage>
+        <ProjectImage loading='eager' className={styles.layout} pictureName={'layout.png'} height={870} width={830} projNumber={1} />
       </article>
       <article className={styles.screen}>
-        <ProjectImage className={styles.interior} pictureName={'interior.jpg'} height={1180} width={783} projNumber={1}></ProjectImage>
-        <ProjectImage className={styles.mirrors} pictureName={'mirrors.png'} height={223} width={835} projNumber={1}></ProjectImage>
+        <ProjectImage loading='eager' className={styles.interior} pictureName={'interior.jpg'} height={1180} width={783} projNumber={1} />
+        <ProjectImage loading='eager' className={styles.mirrors} pictureName={'mirrors.png'} height={223} width={835} projNumber={1} />
       </article>
       <article className={styles.screen}>
-        <ProjectImage className={styles.handle} pictureName={'handle.jpg'} height={607} width={903} projNumber={1}></ProjectImage>
-        <ProjectImage className={styles.blueprint} pictureName={'blueprint.png'} height={870} width={835} projNumber={1}></ProjectImage>
+        <ProjectImage loading='eager' className={styles.handle} pictureName={'handle.jpg'} height={607} width={903} projNumber={1} />
+        <ProjectImage loading='eager' className={styles.blueprint} pictureName={'blueprint.png'} height={870} width={835} projNumber={1} />
       </article>
     </main>
   )
