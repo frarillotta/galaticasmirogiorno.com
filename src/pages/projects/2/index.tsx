8445b0b234aae7d4fe91e75472dc681ddaaf344e
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-05-07 15:10:01 +0100

    better image formats

--- src/pages/projects/2/index.tsx
+++ src/pages/projects/2/index.tsx
@@ -15,22 +15,22 @@ export default function Project2() {
             <br />
             <span>A PAVILLION LOOKING LIKE A TRAIN.</span><br />
           </ProjectPageDescription>
-          <ProjectImage loading='eager' priority={true} className={styles.pavillion} pictureName={'pavillion'} height={350} width={250} projNumber={2} />
-          <ProjectImage loading='eager' priority={true} className={styles.otherside} pictureName={'otherside'} height={450} width={300} projNumber={2} />
+          <ProjectImage loading='eager' priority={true} className={styles.pavillion} pictureName={'pavillion'} height={350} width={500} projNumber={2} />
+          <ProjectImage loading='eager' priority={true} className={styles.otherside} pictureName={'otherside'} height={350} width={500} projNumber={2} />
         </ProjectScreen>
         <ProjectScreen>
-          <ProjectImage loading='eager' className={styles.layout1} pictureName={'layout1'} height={600} width={300} projNumber={2} />
-          <ProjectImage loading='eager' className={styles.interior} pictureName={'interior'} height={450} width={300} projNumber={2} />
+          <ProjectImage loading='eager' className={styles.layout1} pictureName={'layout1'} height={700} width={400} projNumber={2} />
+          <ProjectImage loading='eager' className={styles.interior} pictureName={'interior'} height={500} width={350} projNumber={2} />
         </ProjectScreen>
         <ProjectScreen>
-          <ProjectImage loading='eager' className={styles.side} pictureName={'side'} height={300} width={300} projNumber={2} />
+          <ProjectImage loading='eager' className={styles.side} pictureName={'side'} height={500} width={500} projNumber={2} />
         </ProjectScreen>
         <ProjectScreen>
-          <ProjectImage loading='eager' className={styles.layout2} pictureName={'layout2'} height={300} width={300} projNumber={2} />
-          <ProjectImage loading='eager' className={styles.outside} pictureName={'outside'} height={450} width={300} projNumber={2} />
+          <ProjectImage loading='eager' className={styles.layout2} pictureName={'layout2'} height={380} width={380} projNumber={2} />
+          <ProjectImage loading='eager' className={styles.outside} pictureName={'outside'} height={380} width={580} projNumber={2} />
         </ProjectScreen>
         <ProjectScreen>
-          <ProjectImage loading='eager' className={styles.front} pictureName={'front'} height={450} width={300} projNumber={2} />
+          <ProjectImage loading='eager' className={styles.front} pictureName={'front'} height={450} width={650} projNumber={2} />
         </ProjectScreen>
       </ProjectWrapper>
     )
