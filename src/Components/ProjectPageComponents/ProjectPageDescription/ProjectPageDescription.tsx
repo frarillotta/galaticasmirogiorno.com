1d09265f2c39e7998dfcd352054c5e571fab3d45
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-05-07 17:53:57 +0100

    fix line, add nic emobile styling

--- src/Components/ProjectPageComponents/ProjectPageDescription/ProjectPageDescription.tsx
+++ src/Components/ProjectPageComponents/ProjectPageDescription/ProjectPageDescription.tsx
@@ -1,21 +1,31 @@
 import Image from 'next/image'
 import styles from './ProjectPageDescription.module.css';
 import { AnimatePresence, motion } from 'framer-motion';
+import { Line } from '~/Components/SvgIcons';
+
+
+//workaround for blurriness
+function template({ scaleY }: {scaleY: number}) {
+  return `scaleY(${scaleY})`
+}
 
 export const ProjectPageDescription: React.FC<{ children: React.ReactNode, className?: string }> = ({ children, className = '' }) => {
   return <AnimatePresence>
     <div className={`${styles.description} ${className}`}>
-      <motion.div
-        className={styles.verticalLine}
-        initial={{ scaleY: 0, originY: 0 }}
-        animate={{ scaleY: 4 }}
-        transition={{ duration: 2, ease: 'easeInOut', delay: 0.5 }}
-      >
-        <Image width={4} height={48} alt='vertical line' src="/icons/line.svg" />
-      </motion.div>
-      <p className={styles.descriptionParagraph}>
-        {children}
-      </p>
+      <div className={styles.paragraphWrapper}>
+        <motion.div
+          className={styles.verticalLineWrapper}
+          transformTemplate={template}
+          initial={{ scaleY: 0, transformOrigin: '50% 0% 0px' }}
+          animate={{ scaleY: 1, transformOrigin: '50% 0% 0px' }}
+          transition={{ duration: 2, ease: 'easeInOut', delay: 0.5 }}
+        >
+          <Line className={styles.verticalLine} />
+        </motion.div>
+        <p className={styles.descriptionParagraph}>
+          {children}
+        </p>
+      </div>
     </div>
   </AnimatePresence>
 }
--- src/Components/SvgIcons/Line.tsx
+++ src/Components/SvgIcons/Line.tsx
@@ -2,8 +2,8 @@ import { forwardRef } from "react"
 
 
 export const Line = forwardRef<SVGSVGElement, { className?: string }>(({ className = '' }, ref) => {
-    return <svg ref={ref} className={className} viewBox="0 0 4 100" xmlns="http://www.w3.org/2000/svg">
-        <line x1="0" y1="0" x2="0" y2="100" stroke="black" stroke-width="3" />
+    return <svg preserveAspectRatio="xMidYMid slice" ref={ref} className={className} viewBox="0 0 4 200" xmlns="http://www.w3.org/2000/svg">
+        <line x1="0" y1="0" x2="0" y2="200" stroke="black" stroke-width="3" />
     </svg>
 
 })
