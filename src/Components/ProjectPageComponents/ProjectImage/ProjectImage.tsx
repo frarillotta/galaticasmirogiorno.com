79bb72356d527369bd0ebbb25f98f219e9190adf
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-08-04 00:31:14 +0100

    try

--- src/Components/ProjectPageComponents/ProjectImage/ProjectImage.tsx
+++ src/Components/ProjectPageComponents/ProjectImage/ProjectImage.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './ProjectImage.module.css';
-import { AnimatePresence, Variants, motion } from 'framer-motion';
+import { AnimatePresence, Variants, motion, useAnimate, useInView } from 'framer-motion';
+import { useCallback } from 'react';
 
 type ProjectImageProps = {
     pictureName: string;
@@ -38,11 +39,42 @@ const imageVariants: Variants = {
 }
 
 export const ProjectImage: React.FC<ProjectImageProps> = ({ pictureName, projNumber, width, height, loading, format = 'avif', priority = false, className = '' }) => {
+
+    const [imageScope, animateImage] = useAnimate();
+    const isImageInView = useInView(imageScope);
+    const [barScope, animateBar] = useAnimate();
+    const onLoad = useCallback(() => {
+        if (isImageInView) {
+            animateImage(imageScope.current, {
+                clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+                transition: {
+                    ease: 'easeOut',
+                    duration: 1,
+                    delay: 0.1
+                }
+            });
+        }
+        animateBar(barScope.current, {
+            top: '100%',
+            opacity: 0,
+            transition: {
+                ease: 'easeOut',
+                duration: 1,
+                delay: 0.1,
+                opacity: {
+                    ease: 'easeOut',
+                    duration: 0.45,
+                    delay: 0.1,
+                }
+            }
+        })
+    }, [isImageInView, animateBar, animateImage, barScope, imageScope])
     return <div
         className={`${styles.pictureWrapper} ${className}`}
     >
         <AnimatePresence>
             <MotionImage
+                ref={imageScope}
                 variants={imageVariants}
                 initial={'initial'}
                 whileHover={'whileHover'}
@@ -50,26 +82,14 @@ export const ProjectImage: React.FC<ProjectImageProps> = ({ pictureName, projNum
                 viewport={{ once: true }}
                 loading={loading}
                 priority={priority}
+                onLoad={onLoad}
                 className={styles.projectPicture}
                 alt={`project ${projNumber} ${pictureName} Picture`}
                 src={`/projects/${projNumber}/pictures/${pictureName}.${format}`}
                 width={width}
                 height={height}
             />
-            <motion.div animate={{
-                top: '100%',
-                opacity: 0, 
-                transition: {
-                    ease: 'easeOut',
-                    duration: 1,
-                    delay: 0.1,
-                    opacity: {
-                        ease: 'easeOut',
-                        duration: 0.45,
-                        delay: 0.1,
-                    }
-                }
-            }} className={styles.blackBar} />
+            <div ref={barScope} className={styles.blackBar} />
         </AnimatePresence>
     </div>
 };
