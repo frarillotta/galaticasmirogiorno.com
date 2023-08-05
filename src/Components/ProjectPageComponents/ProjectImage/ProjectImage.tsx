7dd55a18e853271db79233577296f17993e49a38
Francesco Arillotta <francesco.arillotta@thetrainline.com>
2023-08-05 13:52:07 +0100

    reintroduce anmation on viewport

--- src/Components/ProjectPageComponents/ProjectImage/ProjectImage.tsx
+++ src/Components/ProjectPageComponents/ProjectImage/ProjectImage.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './ProjectImage.module.css';
-import { AnimatePresence, Variants, motion, useAnimate } from 'framer-motion';
+import { AnimatePresence, Variants, motion, useAnimate, useInView } from 'framer-motion';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { mouseOutInEventListener } from '~/Components/Cursor/Cursor';
 
@@ -36,15 +36,11 @@ export const ProjectImage: React.FC<ProjectImageProps> = ({ pictureName, projNum
     const imageRef = useRef<HTMLImageElement>(null);
     const [wrapperScope, animateShadow] = useAnimate();
 
-    // const isImageInView = useInView(imageScope);
+    const isImageInView = useInView(imageScope);
     const [rendered, setRendered] = useState(false);
 
     const animateIn = useCallback(() => {
-        if (
-            // isImageInView && 
-            imageRef.current?.complete &&
-            !rendered
-        ) {
+        if (isImageInView && imageRef.current?.complete && !rendered) {
             animateImage(imageScope.current, {
                 clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
             }, {
@@ -62,15 +58,15 @@ export const ProjectImage: React.FC<ProjectImageProps> = ({ pictureName, projNum
             })
             setRendered(true)
         }
-    }, [imageScope, rendered, animateImage, animateShadow, wrapperScope]);
+    }, [isImageInView, imageScope, rendered, animateImage, animateShadow, wrapperScope]);
 
     const onLoadComplete = useCallback(() => {
         animateIn();
     }, [animateIn]);
 
-    // useEffect(() => {
-    //     animateIn();
-    // }, [animateIn]);
+    useEffect(() => {
+        animateIn();
+    }, [animateIn]);
 
     useEffect(() => {
 
