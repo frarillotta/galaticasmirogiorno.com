02ae88faf269853934d632e5ba8bd512b17d2fd8
frarillotta <frarillotta@gmail.com>
2025-03-27 12:00:37 +0000

    fix line

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,30 +1,9 @@
 import styles from './Header.module.css'
 import { Routes } from '../../constants';
-import { useRouter } from 'next/router';
-import { useAnimate } from 'framer-motion';
-import { useEffect } from 'react';
 import { Line } from '../SvgIcons';
 import { Link } from '../Link/Link';
 
 export const Header = () => {
-    const router = useRouter();
-    const [scope, animate] = useAnimate();
-    
-    useEffect(() => {
-        if (router.asPath === Routes.Home) {
-            animate(scope.current, {
-                originY: 0,
-                scaleY: 0,
-            }, {delay: 0.49, ease: 'linear', duration: 0.1})
-        } else {
-            animate(scope.current, {
-                originY: 0,
-                scaleY: 1,
-                //workaround cause safari sucks
-                z: 0.1,
-            })
-        }
-    }, [router, animate, scope])
 
     return <header className={styles.header}>
         <Link ariaLabel="RUGGIERO GALATI CASMIRO CLAUDIA GIORNO" href={Routes.Home} className={styles.namesWrapper}>
@@ -36,7 +15,7 @@ export const Header = () => {
             </h1>
         </Link>
         <div className={styles.lineWrapper}>
-            <Line className={styles.verticalLine} ref={scope} />
+            <Line className={styles.verticalLine} />
         </div>
             <h1 className={styles.label}>
                 ARCHITECTURE
