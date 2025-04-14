d44cdc23fb44b540e7a4a6cdfb3540b3642b8d54
frarillotta <frarillotta@gmail.com>
2025-04-14 16:53:34 +0100

    fix line

--- src/Components/SvgIcons/Line.tsx
+++ src/Components/SvgIcons/Line.tsx
@@ -2,14 +2,24 @@ import { forwardRef } from "react"
 
 
 export const Line = forwardRef<SVGSVGElement, { className?: string }>(({ className = '' }, ref) => {
-    return <svg 
-        preserveAspectRatio="xMidYMid slice" 
-        ref={ref} 
-        className={className} 
-        viewBox="0 0 4 200" 
+    return <svg
+        preserveAspectRatio="xMidYMid slice"
+        ref={ref}
+        className={className}
+        viewBox="0 0 3 400"
         xmlns="http://www.w3.org/2000/svg"
     >
-        <line vectorEffect={'non-scaling-stroke'} x1="0" y1="0" x2="0" y2="200" stroke="black" strokeWidth="3" />
+        <line
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="400"
+            stroke="black"
+            strokeWidth="30"
+            shape-rendering="crispEdges"
+            vectorEffect='non-scaling-stroke'
+            stroke-alignment="inside"
+        />
     </svg>
 
 })
