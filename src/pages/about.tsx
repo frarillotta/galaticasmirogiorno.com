3a734aade2b98dfef3f8026bc19deb028b4d868a
frarillotta <frarillotta@gmail.com>
2025-09-27 13:43:57 +0100

    prova

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,7 +9,10 @@ export default function About() {
   return (
     <main className={styles.main}>
 
-      <h1 className={styles.aboutTitle}>ARE TWO ARCHITECTS FROM SOUTHERN ITALY.</h1>
+      <h1 className={styles.aboutTitle}>are two architects who met in Calabria, Southern Italy. Their story is deeply in the South: one from Calabria, the other from Sicily - regions divided by the wild landscape of Strait of Messina. <br/>
+      They graduated together in 2020 with a thesis on the creative reuse of unfinished architecture, the most defining phenomena in their territories. <br/>
+      Today, they live and work at the foot of Mount Etna, embracing slow living and nature.
+      </h1>
       <div className={styles.iconWrapper} >
         <AboutIcon className={styles.icon} />
       </div>
